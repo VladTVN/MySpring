ef687b936207464bcc8077ae644ea8b308999188
VladTVN <vlad-tereshko@tut.by>
2020-09-28 15:15:48 +0300

    Spring Web-application with JPA(Hibernate)

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -1,13 +1,23 @@
 package com.example;
 
+import com.example.domain.Message;
+import com.example.repos.MessageRepo;
+import org.aspectj.bridge.IMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required = false,defaultValue = "World") String name, Map<String,Object> model){
@@ -16,11 +26,31 @@ public class GreetingController {
     }
 
     @GetMapping
-    public static String main(Map<String,Object> model) {
-        model.put("some","My method");
+    public  String main(Map<String,Object> model) {
+       Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages",messages);
         return "main";
     }
 
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String,Object> model){
+        Message message = new Message(text,tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages",messages);
+        return "main";
+    }
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String,Object> model){
+       Iterable<Message> messages;
+        if(filter!=null && !filter.isEmpty()){
+        messages = messageRepo.findByTag(filter);
+        }else{
+            messages= messageRepo.findAll();
+        }
 
+        model.put("messages",messages);
+        return "main";
+    }
 
 }
