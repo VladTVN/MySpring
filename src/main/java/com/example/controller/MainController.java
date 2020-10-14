4d1610289947f7fd07796835af943657926716ad
VladTVN <vlad-tereshko@tut.by>
2020-10-14 12:51:20 +0300

    Added authentication and registration

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,38 +1,37 @@
-package com.example;
+package com.example.controller;
 
 import com.example.domain.Message;
 import com.example.repos.MessageRepo;
-import org.aspectj.bridge.IMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+
 import java.util.Map;
-import java.util.Scanner;
+
 
 @Controller
-public class GreetingController {
+public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required = false,defaultValue = "World") String name, Map<String,Object> model){
-       model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String,Object> model){
        return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public  String main(Map<String,Object> model) {
        Iterable<Message> messages = messageRepo.findAll();
         model.put("messages",messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String,Object> model){
         Message message = new Message(text,tag);
         messageRepo.save(message);
