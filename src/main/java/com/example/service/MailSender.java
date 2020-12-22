ea305b1ccec1dd0412c5067fe3ec781cbda7cd30
VladTVN <vlad-tereshko@tut.by>
2020-12-22 21:31:36 +0300

    Added: activation user by activation code

---
+++ src/main/java/com/example/service/MailSender.java
@@ -0,0 +1,32 @@
+package com.example.service;
+
+import freemarker.template.utility.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailMessage;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSender {
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void send(String emailTo, String subject, String message){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(username);
+        mailMessage.setTo(emailTo);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
+    }
+
+
+}
