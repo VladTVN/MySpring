c4a60d2c5367f91b4c43b0f31e524abad74def45
VladTVN <vlad-tereshko@tut.by>
2020-12-25 15:31:06 +0300

    Added: bean validation, password encryption

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,4 +1,4 @@
 insert into springapp_db.usr (id, username, password, active)
-    values (1,'admin','admin',true);
+    values (1,'admin','$2a$08$SEa9im5GKA9uJF9uafhyJOhPZB9ivQ2e8ajK2w6f2NFDkDCBbOtKq',true);
 insert into springapp_db.user_role (user_id, roles)
 values (1,'USER'), (1,'ADMIN');
